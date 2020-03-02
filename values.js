3384697ee57a5d063bf3f18c78dff7cb2125d037
beljjvalentin <30524956+beljjvalentin@users.noreply.github.com>
2020-03-02 11:58:42 +0200

    Update values.js
    March data setup

--- values.js
+++ values.js
@@ -1,5 +1,5 @@
 // Global date variables
 var year = 2020;
-var month = 2;
+var month = 3;
 // File urls
-var Schedule_url = "10uuLwFPtgKMfbqoJXBMy2cEuu43hS_Y5cjnCDVsdJo0";
+var Schedule_url = "1ypvdfNjQbxzf7RCpNrtdgDdxLXx7VjSATX5mOL0nNag";
