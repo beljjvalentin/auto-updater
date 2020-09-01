947d3eb81f1e9502ed5ed94463b82bee9a62c492
beljjvalentin <30524956+beljjvalentin@users.noreply.github.com>
2020-09-01 11:36:17 +0300

    Sept changes

--- values.js
+++ values.js
@@ -1,5 +1,5 @@
 // Global date variables
 var year = 2020;
-var month = 8;
+var month = 9;
 // File urls
-var Schedule_url = "1PXUYC1BBOPHSZahjd5GvXQViB3wo2Mq5AybK-kyDZDY";
+var Schedule_url = "1BXYE_0-8x6FZ910Kl0-WTEaBmQrDKJqqJXJWETkvRPw";
