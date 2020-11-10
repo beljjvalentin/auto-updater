43abaf12fb8155d12814750e7fcabe0620a0ff4f
beljjvalentin <30524956+beljjvalentin@users.noreply.github.com>
2020-11-10 11:13:33 +0200

    Update Schedule.js

--- Schedule.js
+++ Schedule.js
@@ -516,12 +516,43 @@ function pushQApnl(order, offer_id){
 						break;
 				} 
 				if(data.response.data.data[i].Stat.conversions > 0 && data.response.data.data[i].Offer.name.includes( offer_id )){
-					//console.log(writer4[c][0]);
-					//console.log(data.response.data.data[i]);
 					if(writer4[c][(QAoffers.length+order+1)*2+1] == undefined) 
 						writer4[c][(QAoffers.length+order+1)*2+1] = parseInt(data.response.data.data[i].Stat.conversions);
 					else
 						writer4[c][(QAoffers.length+order+1)*2+1] = parseInt(writer4[c][(QAoffers.length+order+1)*2+1]) + parseInt(data.response.data.data[i].Stat.conversions);
+					/*if(writer4[c][(QAoffers.length+order+1)*2+2] == undefined) 
+						writer4[c][(QAoffers.length+order+1)*2+2] = parseInt(data.response.data.data[i].Stat.payout);
+					else 
+						writer4[c][(QAoffers.length+order+1)*2+2] = parseInt(writer4[c][(QAoffers.length+order+1)*2+2]) + parseInt(data.response.data.data[i].Stat.payout);*/
+				}
+			}	
+		}catch(e){
+			
+		}
+		if(order == pushQAoffers.length-1) showHint6(1);
+	}
+	};
+	xhttp.open("GET", "https://psflc.api.hasoffers.com/Apiv3/json?NetworkToken=NETvgwPirxWahAF3mj5WHJs2HT5tLv&Target=Report&Method=getStats&fields[]=Stat.date&fields[]=Offer.name&fields[]=Stat.clicks&fields[]=Stat.conversions&fields[]=Stat.revenue&fields[]=Stat.payout&filters[Goal.name][conditional]=LIKE&filters[Goal.name][values]=lead&filters[Stat.date][conditional]=BETWEEN&filters[Stat.date][values][]="+year+"-"+month+"-01&filters[Stat.date][values][]="+year+"-"+month+"-"+days+"&filters[Offer.name][conditional]=LIKE&filters[Offer.name][values][]=%Push+Flow%&limit=1000", true);
+	setTimeout(xhttp.send(), 100);
+	
+	xhttp2 = new XMLHttpRequest();
+	xhttp2.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		try{
+			var data = JSON.parse(this.responseText);
+			if(date_count<data.response.data.data.length)
+				date_count = data.response.data.data.length;
+			var c = 0;
+			for(var i=0; i<data.response.data.data.length; i++){	
+				for(c = 0; c<31; c++){
+					if(writer4[c][0] == data.response.data.data[i].Stat.date)
+						break;
+				} 
+				if(data.response.data.data[i].Stat.conversions > 0 && data.response.data.data[i].Offer.name.includes( offer_id )){
+					/*if(writer4[c][(QAoffers.length+order+1)*2+1] == undefined) 
+						writer4[c][(QAoffers.length+order+1)*2+1] = parseInt(data.response.data.data[i].Stat.conversions);
+					else
+						writer4[c][(QAoffers.length+order+1)*2+1] = parseInt(writer4[c][(QAoffers.length+order+1)*2+1]) + parseInt(data.response.data.data[i].Stat.conversions);*/
 					if(writer4[c][(QAoffers.length+order+1)*2+2] == undefined) 
 						writer4[c][(QAoffers.length+order+1)*2+2] = parseInt(data.response.data.data[i].Stat.payout);
 					else 
@@ -534,8 +565,8 @@ function pushQApnl(order, offer_id){
 		if(order == pushQAoffers.length-1) showHint6(1);
 	}
 	};
-	xhttp.open("GET", "https://psflc.api.hasoffers.com/Apiv3/json?NetworkToken=NETvgwPirxWahAF3mj5WHJs2HT5tLv&Target=Report&Method=getStats&fields[]=Stat.date&fields[]=Offer.name&fields[]=Stat.clicks&fields[]=Stat.conversions&fields[]=Stat.revenue&fields[]=Stat.payout&filters[Goal.name][conditional]=LIKE&filters[Goal.name][values]=LEAD_SOI&filters[Stat.date][conditional]=BETWEEN&filters[Stat.date][values][]="+year+"-"+month+"-01&filters[Stat.date][values][]="+year+"-"+month+"-"+days+"&filters[Offer.name][conditional]=LIKE&filters[Offer.name][values][]=%Push+Flow%&limit=1000", true);
-	setTimeout(xhttp.send(), 100);
+	xhttp2.open("GET", "https://psflc.api.hasoffers.com/Apiv3/json?NetworkToken=NETvgwPirxWahAF3mj5WHJs2HT5tLv&Target=Report&Method=getStats&fields[]=Stat.date&fields[]=Offer.name&fields[]=Stat.clicks&fields[]=Stat.conversions&fields[]=Stat.revenue&fields[]=Stat.payout&filters[Stat.date][conditional]=BETWEEN&filters[Stat.date][values][]="+year+"-"+month+"-01&filters[Stat.date][values][]="+year+"-"+month+"-"+days+"&filters[Offer.name][conditional]=LIKE&filters[Offer.name][values][]=%Push+Flow%&limit=1000", true);
+	setTimeout(xhttp2.send(), 100);
 }
  
 function showHint6(activate) {
