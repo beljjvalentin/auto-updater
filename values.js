c9a82b9c78a36e8aed89c6542eab161f6bd2e40c
beljjvalentin <30524956+beljjvalentin@users.noreply.github.com>
2020-12-01 12:02:59 +0200

    Update values.js

--- values.js
+++ values.js
@@ -1,5 +1,5 @@
 // Global date variables
 var year = 2020;
-var month = 11;
+var month = 12;
 // File urls
-var Schedule_url = "1TJ4DnWFlA3Nj5nVAMCMj8E86cl1LkQhsXLVvJoscVOY";
+var Schedule_url = "1WIaq5roOPh29g9-FZCpMtGGUW6xK5FYEW-3mRsApuAc";
